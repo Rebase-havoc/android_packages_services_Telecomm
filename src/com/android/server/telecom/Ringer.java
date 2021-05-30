pimpmaneaton/platform_packages_services_Telecomm

Telecomm: Added features flash-call-on-waiting [2/3]
Reworked to follow the same logic as flashlight blink for incoming call

src/com/android/server/telecom/Ringer.java
@@ -176,6 +176,7 @@ public class Ringer {
     private boolean mIsVibrating = false;
 
     private int torchMode;
+    private boolean mFlashOnCallWait;
 
     /** Initializes the Ringer. */
     @VisibleForTesting
@@ -475,6 +476,13 @@ public class Ringer {
             vibrate(200, 300, 500);
         }
 
+        mFlashOnCallWait = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.FLASHLIGHT_ON_CALL_WAITING, 0, UserHandle.USER_CURRENT) == 1;
+
+        if (mFlashOnCallWait) {
+            blinkFlashlight();
+        }
+
         if (mCallWaitingPlayer == null) {
             Log.addEvent(call, LogUtils.Events.START_CALL_WAITING_TONE, reason);
             mCallWaitingCall = call;
@@ -517,6 +525,13 @@ public class Ringer {
 
             mCallWaitingPlayer.stopTone();
             mCallWaitingPlayer = null;
+        }
+
+        mFlashOnCallWait = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.FLASHLIGHT_ON_CALL_WAITING, 0, UserHandle.USER_CURRENT) == 1;
+
+        if (mFlashOnCallWait) {
+            torchToggler.stop();
         }
     }
 

Rebase-havoc/android_packages_services_Telecomm

Change-Id: Id3cbe75519a50326573032f374af849438c9eb9f

src/com/android/server/telecom/Ringer.java
@@ -245,6 +245,7 @@ public class Ringer {
     private boolean mIsVibrating = false;
 
     private int torchMode;
+    private boolean mFlashOnCallWait;
 
     /** Initializes the Ringer. */
     @VisibleForTesting
@@ -547,6 +548,13 @@ public class Ringer {
             }
         }
 
+        mFlashOnCallWait = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.FLASH_ON_CALL_WAITING, 0, UserHandle.USER_CURRENT) == 1;
+
+        if (mFlashOnCallWait) {
+            blinkFlashlight();
+        }
+
         if (mCallWaitingPlayer == null) {
             Log.addEvent(call, LogUtils.Events.START_CALL_WAITING_TONE, reason);
             mCallWaitingCall = call;
@@ -589,6 +597,13 @@ public class Ringer {
 
             mCallWaitingPlayer.stopTone();
             mCallWaitingPlayer = null;
+        }
+
+        mFlashOnCallWait = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.FLASH_ON_CALL_WAITING, 0, UserHandle.USER_CURRENT) == 1;
+
+        if (mFlashOnCallWait) {
+            torchToggler.stop();
         }
     }
 